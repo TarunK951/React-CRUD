fafdd9af50b934fbb9b5e085b85e69b69a3bbcd4
Tarun <119518924+TarunK951@users.noreply.github.com>
2025-03-31 20:28:15 +0530

    popUp

--- slamBook/src/comp2/Input.jsx
+++ slamBook/src/comp2/Input.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./input.css";
 function Input(props) {
   //
   const [value, setValue] = React.useState("");
+  const [pop, setPop] = useState(false);
   //   const data = [];
 
   const Add = () => {
     // console.log(value);
-    const data = { id: props.list.length + 1, name: value, work: "not done" };
+    const data = {
+      id: props.list.length + 1,
+      name: value,
+      work: "not done",
+      imp: props.check,
+    };
     props.setList([...props.list, data]);
+    props.setCheck(false);
     console.log(props.list);
     setValue("");
   };
+
+  React.useEffect(() => {
+    console.log("Updated List:", props.list);
+  }, [props.list]);
 
   return (
     <div>
@@ -20,13 +31,22 @@ function Input(props) {
           <h1>Tasks</h1>
         </div>
         <div className="input">
-          <input
-            type="text"
-            id="inp"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <button id="addBtn" onClick={Add}>
+          {/* <button id="addBtn" onClick={Add}>
+            <div className="add">
+              <p>ADD</p>
+              <img
+                src="https://img.icons8.com/?size=100&id=24717&format=png&color=000000"
+                alt="plus"
+                width="24"
+                height="24"
+              />
+            </div>
+          </button> */}
+          <button
+            onClick={() => {
+              setPop(true);
+            }}
+          >
             <div className="add">
               <p>ADD</p>
               <img
@@ -37,6 +57,72 @@ function Input(props) {
               />
             </div>
           </button>
+          {/*  */}
+          {pop && (
+            <div className="pop">
+              <div className="closeText">
+                <button onClick={() => setPop(false)} className="close">
+                  X
+                </button>
+              </div>
+              <input
+                className="textContainer"
+                type="text"
+                id="inp"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+              {/* important insertion */}
+              <div className="important">
+                <label htmlFor="imp" className="impTitle">
+                  important
+                </label>
+                <input
+                  id="imp"
+                  type="checkBox"
+                  className="impBox"
+                  checked={props.check}
+                  onChange={(e) => {
+                    e.target.value;
+                  }}
+                  onClick={() => {
+                    props.setCheck((prev) => !prev);
+                    console.log("check", props.check);
+                  }}
+                />
+              </div>
+              <div className="important">
+                <label htmlFor="imp" className="impTitle">
+                  confidential
+                </label>
+                <input
+                  id="secret"
+                  type="checkBox"
+                  className="impBox"
+                  checked={props.secret}
+                  onChange={(e) => {
+                    e.target.value;
+                  }}
+                  onClick={() => {
+                    props.setSecret((prev) => !prev);
+                    console.log("secret", props.secret);
+                  }}
+                />
+              </div>
+
+              <button id="addBtn" onClick={Add}>
+                <div className="add">
+                  <p>ADD</p>
+                  <img
+                    src="https://img.icons8.com/?size=100&id=24717&format=png&color=000000"
+                    alt="plus"
+                    width="24"
+                    height="24"
+                  />
+                </div>
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- slamBook/src/comp2/Parent.jsx
+++ slamBook/src/comp2/Parent.jsx
@@ -10,20 +10,59 @@ function Parent() {
       id: 1,
       name: "hi",
       work: "done",
+      imp: false,
     },
     {
       id: 2,
       name: "hello",
       work: "not done",
+      imp: false,
     },
   ]);
+
+  const [sData, setSdata] = useState([
+    {
+      id: 1,
+      name: "hi",
+      work: "done",
+      imp: false,
+    },
+    {
+      id: 1,
+      name: "hi",
+      work: "done",
+      imp: false,
+    },
+  ]);
+
+  const [secret, setSecret] = useState(false);
+
+  console.log(list);
+
+  const [check, setCheck] = useState(false);
 
   const [done, setDone] = useState([]);
 
   return (
     <div>
-      <Input list={list} setList={setList} />
-      <Display list={list} setList={setList} done={done} setDone={setDone} />
+      <Input
+        list={list}
+        setList={setList}
+        check={check}
+        setCheck={setCheck}
+        secret={secret}
+        setSecret={setSecret}
+      />
+      <Display
+        list={list}
+        setList={setList}
+        done={done}
+        setDone={setDone}
+        check={check}
+        setCheck={setCheck}
+        secret={secret}
+        setSecret={setSecret}
+      />
       <Done done={done} setDone={setDone} list={list} setlist={setList} />
     </div>
   );
